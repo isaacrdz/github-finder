ab7f884962cf79d3141ab74a0d86a54f115929d1
isaacrdz <zack_cpp@hotmail.com>
2020-03-21 11:31:38 -0500

    starting hooks

---
+++ src/components/users/Search.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Search extends Component {
+  state = {
+    text: ""
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "ligth");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className="form">
+          <input
+            type="text"
+            name="text"
+            placeholder="Search users"
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input type="submit" className="btn btn-dark btn-block" />
+        </form>
+        {this.props.showClear && (
+          <button
+            className="btn btn-light btn-block"
+            onClick={this.props.clearUsers}
+          >
+            Clear
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+export default Search;
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        className="round-img"
+        alt=""
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+          More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default UserItem;
